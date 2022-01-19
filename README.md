09a98622c14bf23b81c382249ca8e32ca1639a15
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-01-19 23:56:12 +0000

    screenshots and readme update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Python-Projects
-A random assortment of python projects
+# Project List
+* Currency Converter
+
+# Currency Converter
+<h3 align="center">
+  The main window of Currency Converter
+</h3>
+<p align="center">
+  <img src="screenshots\currency converter.png">
+</p>
+
+<h3 align="center">
+  The main window of Currency Converter
+</h3>
+<p align="center">
+  <img src="screenshots\currency converter info.png">
+</p>
+
+<h3 align="center">
+  The main window of Currency Converter
+</h3>
+<p align="center">
+  <img src="screenshots\currency converter update.png">
+</p>
--- currency converter/forexData.py
+++ currency converter/forexData.py
@@ -1,4 +1,4 @@
-updateDate = '2022-01-19 23:11:02'
+updateDate = '2022-01-19 23:46:45'
 
 currencyNames = ['AUD — Australian Dollar', 'BGN — Bulgarian Lev', 'BRL — Brazilian Real', 'CAD — Canadian Dollar', 'CHF — Swiss Franc', 'CNY — Chinese/Yuan Renminbi', 'CZK — Czech Koruna', 'DKK — Danish Krone', 'EUR — European Euro', 'GBP — British Pound', 'HKD — Hong Kong Dollar', 'HRK — Croatian Kuna', 'HUF — Hungarian Forint', 'IDR — Indonesian Rupiah', 'ILS — Israeli New Sheqel', 'INR — Indian Rupee', 'ISK — Icelandic Króna', 'JPY — Japanese Yen', 'KRW — South Korean Won', 'MXN — Mexican Peso', 'MYR — Malaysian Ringgit', 'NOK — Norwegian Krone', 'NZD — New Zealand Dollar', 'PHP — Philippine Peso', 'PLN — Polish Zloty', 'RON — Romanian Leu', 'RUB — Russian Ruble', 'SEK — Swedish Krona', 'SGD — Singapore Dollar', 'THB — Thai Baht', 'TRY — Turkish New Lira', 'USD — United States Dollar', 'ZAR — South African Rand']
 
